a8ff98a23789268285eb2b720c97456880ba9d49
Filipe Braida <filipebraida@gmail.com>
2017-04-19 19:12:02 -0300

    Change the example

--- examples/kfold.jl
+++ examples/kfold.jl
@@ -4,7 +4,7 @@ cf = Persa
 ds = cf.createdummydataset()
 
 for (ds_train, ds_test) in cf.KFolds(ds, 10)
-  model = cf.SurpriseKNNWithMeans(ds_train; k = 60, min_k = 60)
+  model = cf.ImprovedRegularizedSVD(ds_train)
 
   cf.train!(model, ds_train)
 
