414dd2c67f171818668a0f3ae8545e3af06686b0
Filipe Braida <filipebraida@gmail.com>
2017-04-19 14:21:20 -0300

    Better dummy dataset

--- test/datasetTest.jl
+++ test/datasetTest.jl
@@ -1,16 +1,15 @@
-#MovieLens
-ds = cf.MovieLens()
+ds = cf.createdummydataset()
 
-@test ds.users == 943
-@test ds.items == 1682
-@test length(ds.preferences.possibles) == 5
+@test ds.users == 7
+@test ds.items == 6
+@test length(ds.preferences.possibles) == 9
 @test ds.preferences.min == 1
 @test ds.preferences.max == 5
 
 for (ds_train, ds_test) in cf.KFolds(ds, 10)
-  @test ds_train.users == 943
-  @test ds_train.items == 1682
-  @test length(ds_train.preferences.possibles) == 5
+  @test ds_train.users == 7
+  @test ds_train.items == 6
+  @test length(ds_train.preferences.possibles) == 9
   @test ds_train.preferences.min == 1
   @test ds_train.preferences.max == 5
   @test length(ds_train) != length(ds)
@@ -21,32 +20,11 @@ end
 holdout = cf.HoldOut(ds, 0.9)
 
 (ds_train, ds_test) = cf.get(holdout)
-@test ds_train.users == 943
-@test ds_train.items == 1682
-@test length(ds_train.preferences.possibles) == 5
+@test ds_train.users == 7
+@test ds_train.items == 6
+@test length(ds_train.preferences.possibles) == 9
 @test ds_train.preferences.min == 1
 @test ds_train.preferences.max == 5
 @test length(ds_train) != length(ds)
 @test length(ds_train) != size(ds_test)[1]
 @test length(ds) != size(ds_test)[1]
-
-ds = cf.FilmTrust()
-@test ds.users == 1508
-@test ds.items == 2071
-@test length(ds.preferences.possibles) == 8
-@test ds.preferences.min == 0.5
-@test ds.preferences.max == 4.0
-
-ds = cf.CiaoDVD()
-@test ds.users == 17615
-@test ds.items == 16121
-@test length(ds.preferences.possibles) == 5
-@test ds.preferences.min == 1
-@test ds.preferences.max == 5
-
-ds = cf.MovieTweeting()
-@test ds.users == 25011
-@test ds.items == 14732
-@test length(ds.preferences.possibles) == 11
-@test ds.preferences.min == 0
-@test ds.preferences.max == 10
