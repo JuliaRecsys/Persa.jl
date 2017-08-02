7b8af01648176f3a034687a1171e819ed8eca98a
Filipe Braida <filipebraida@gmail.com>
2017-08-02 10:09:43 -0300

    Adding doc to global mean

--- src/globalMean.jl
+++ src/globalMean.jl
@@ -2,6 +2,21 @@ mutable struct GlobalMean <: CFModel
   μ::Float64
 end
 
+"""
+    GlobalMean(dataset::CFDatasetAbstract)
+
+Collaborative filtering baseline model that use a global mean to predict all the
+ratings.
+
+# Examples
+```julia-repl
+julia> model = Persa.GlobalMean(ds)
+Persa.GlobalMean
+  μ → 3.53…
+julia> Persa.predict(model, 1, 1)
+3.53…
+```
+"""
 GlobalMean{T<:CFDatasetAbstract}(dataset::T) = GlobalMean(mean(dataset))
 
 train!{T<:CFDatasetAbstract}(model::GlobalMean, dataset::T)::ModelStatistic = ModelStatistic()
