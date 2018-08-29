859fb67ffe9c5abe5b3f5eea2e385cca0b0cbf95
Filipe Braida <filipebraida@gmail.com>
2018-08-29 16:28:17 -0300

    Starting the project

--- src/Persa.jl
+++ src/Persa.jl
@@ -2,17 +2,4 @@ module Persa
 
 # package code goes here
 
-include("datasets.jl")
-
-include("learn.jl")
-
-include("kfolds.jl")
-include("measures.jl")
-
-include("utils.jl")
-
-include("globalMean.jl")
-
-include("holdout.jl")
-
 end # module
