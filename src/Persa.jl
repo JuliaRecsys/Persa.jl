8191af11632379bc905af7273b1870eb0d655b40
Filipe Braida <filipebraida@gmail.com>
2018-08-29 22:59:27 -0300

    Create first and autoboxing working

--- src/Persa.jl
+++ src/Persa.jl
@@ -1,5 +1,24 @@
 module Persa
 
 # package code goes here
+import Base: convert, promote_rule
 
+struct ID
+    value::Int
+end
+
+struct Rating{T <: Number}
+    user::ID
+    item::ID
+    value::T
+end
+
+Rating(user, item, value::T) where T <: Number = Rating{T}(user, item, value)
+
+struct Dataset{T <: Number}
+    ratings::Array{Rating{T}}
+end
+
+Base.promote_rule(::Type{ID}, ::Type{Int}) = ID
+Base.convert(::Type{Persa.ID}, x::Int) = Persa.ID(x)
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,38 @@
 using Persa
 using Test
+
+dataset[1] # (1, 1, 5, 2, NaN, 2, NaN)
+
+dataset[1,1] # (Id(1), Id(1), Rating(5, Preferences))
+
+dataset[1,2,1] # Rating(2, Preferences)
+
+dataset[1,1,:] # Context(Rating(2, Preferences), 5, 2, NaN, 2, NaN)
+
+dataset[1,1,:] # Context(Null)
+
+dataset[1,1,:date] # date
+
+contexts(dataset) # [:rating, :date, :day, :humor]
+
+Rating(2, Preferences) + Rating(2, Preferences) = Rating(4, Preferences)
+
+Rating(2, Preferences{Int}) + 2 = Rating(4, Preferences)
+
+Rating(2, Preferences{Int}) + 2.1 = 4.1
+
+Rating(2, Preferences) + 6 = Rating(5, Preferences)
+
+mean([Rating(2, Preferences{Int}), ...]) = 3.5
+
+mean([Rating(2, Preferences{Int}), Rating(Vazio, Preferences{Int}), ...]) = 3.0
+
+Rating(2, Preference) > 2 # false
+
+Rating(2, Preference) + 2 > 2 # true
+
+Rating(2, Preference) + Rating(2, Preference) > 2 # true
+
+Rating(2, Preference) > Rating(2, Preference) # false
+
+Rating(Vazio, Preference) + 1 # Rating(Vazio, Preference)
