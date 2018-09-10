ee4c5e14fe6ad8f11a0743aca70041fe86e1a4d8
Filipe Braida <filipebraida@gmail.com>
2018-09-10 14:18:14 -0300

    First stable version of Persa 2.0

--- src/Persa.jl
+++ src/Persa.jl
@@ -1,24 +1,8 @@
 module Persa
 
 # package code goes here
-import Base: convert, promote_rule
+include("preference.jl")
+include("rating.jl")
+include("dataset.jl")
 
-struct ID
-    value::Int
-end
-
-struct Rating{T <: Number}
-    user::ID
-    item::ID
-    value::T
-end
-
-Rating(user, item, value::T) where T <: Number = Rating{T}(user, item, value)
-
-struct Dataset{T <: Number}
-    ratings::Array{Rating{T}}
-end
-
-Base.promote_rule(::Type{ID}, ::Type{Int}) = ID
-Base.convert(::Type{Persa.ID}, x::Int) = Persa.ID(x)
 end # module
---
+++ src/rating.jl
@@ -0,0 +1,23 @@
+abstract type AbstractRating{T <: Number}
+end
+
+struct Rating{T <: Number} <: AbstractRating{T}
+    value::T
+    preference::Preference{T}
+end
+
+struct MissingRating{T <: Number} <: AbstractRating{T}
+end
+
+Rating(value::T, rating::Rating{T}) where T <: Number = Rating{T}(value, rating.preference)
+
+value(rating::Rating) = rating.value
+value(rating::MissingRating) = missing
+
+Base.zero(::Type{AbstractRating{T}}) where T <: Number = MissingRating{T}()
+
+function convert(values::Array{T})::Array{AbstractRating{T}} where T <: Number
+    preferences = Preference(values)
+
+    [Rating(value, preferences) for value in values]
+end
