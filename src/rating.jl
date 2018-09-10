5d1c593ea2892717fd64758fd30bfc91c08fa22a
Filipe Braida <filipebraida@gmail.com>
2018-09-10 18:24:07 -0300

    create op with ratings

--- src/rating.jl
+++ src/rating.jl
@@ -4,6 +4,7 @@ end
 struct Rating{T <: Number} <: AbstractRating{T}
     value::T
     preference::Preference{T}
+    Rating(value::T, preference::Preference{T}) where T <: Number = new{T}(correct(value, preference), preference)
 end
 
 struct MissingRating{T <: Number} <: AbstractRating{T}
@@ -24,3 +25,18 @@ function convert(values::Array{T})::Array{AbstractRating{T}} where T <: Number
 
     [Rating(value, preferences) for value in values]
 end
+
+Base.:+(r1::Rating{T}, r2::Rating{T}) where {T <: Number} = Rating(value(r1) + value(r2), r1.preference)
+Base.:+(r1::MissingRating{T}, r2::Rating{T}) where {T <: Number} = Rating(value(r2), r2.preference)
+Base.:+(r1::Rating{T}, r2::MissingRating{T}) where {T <: Number} = Rating(value(r1), r1.preference)
+Base.:+(r1::MissingRating{T}, r2::MissingRating{T}) where {T <: Number} = MissingRating()
+
+Base.:-(r1::Rating{T}, r2::Rating{T}) where {T <: Number} = Rating(value(r1) - value(r2), r1.preference)
+Base.:-(r1::MissingRating{T}, r2::Rating{T}) where {T <: Number} = Rating(value(r2), r2.preference)
+Base.:-(r1::Rating{T}, r2::MissingRating{T}) where {T <: Number} = Rating(value(r1), r1.preference)
+Base.:-(r1::MissingRating{T}, r2::MissingRating{T}) where {T <: Number} = MissingRating()
+
+Base.:+(r1::Rating{T}, x::Number) where {T <: Number} = value(r1) + x
+Base.:-(r1::Rating{T}, x::Number) where {T <: Number} = value(r1) - x
+Base.:*(r1::Rating{T}, x::Number) where {T <: Number} = value(r1) * x
+Base.:/(r1::Rating{T}, x::Number) where {T <: Number} = value(r1) / x
