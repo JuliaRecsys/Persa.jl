508b61e2d9a395068d2a0e9f8d49f92eb178d405
Filipe Braida <filipebraida@gmail.com>
2018-09-10 17:14:00 -0300

    Adding isnan function

--- src/rating.jl
+++ src/rating.jl
@@ -14,6 +14,9 @@ Rating(value::T, rating::Rating{T}) where T <: Number = Rating{T}(value, rating.
 value(rating::Rating) = rating.value
 value(rating::MissingRating) = missing
 
+Base.isnan(rating::Rating{T}) where T <: Number = false
+Base.isnan(rating::MissingRating{T}) where T <: Number = true
+
 Base.zero(::Type{AbstractRating{T}}) where T <: Number = MissingRating{T}()
 
 function convert(values::Array{T})::Array{AbstractRating{T}} where T <: Number
