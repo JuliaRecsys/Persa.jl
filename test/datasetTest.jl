45da48baed6605f8279c4884783ec9bea3d4f700
Filipe Braida <filipebraida@gmail.com>
2018-09-10 14:35:38 -0300

    Fists tests of dataset

---
+++ test/datasetTest.jl
@@ -0,0 +1,51 @@
+@testset "Datasets Kernel Tests" begin
+    df1 = createdummydatasetone()
+    dataset1 = Persa.Dataset(df1)
+
+    df2 = createdummydatasettwo()
+    dataset2 = Persa.Dataset(df2, 10, 10)
+
+    @testset "Dummy Tests" begin
+        @test dataset1.users == 7
+        @test dataset1.items == 6
+        @test Persa.users(dataset1) == 7
+        @test Persa.items(dataset1) == 6
+
+        @test dataset2.users == 10
+        @test dataset2.items == 10
+        @test Persa.users(dataset2) == 10
+        @test Persa.items(dataset2) == 10
+
+        @test Persa.size(dataset1) == [7 6]
+        @test Persa.size(dataset2) == [10 10]
+
+        @test Persa.length(dataset1) == 35
+        @test Persa.length(dataset2) == 35
+    end
+
+    @testset "Index Tests" begin
+        for i = 1:size(df1)[1]
+            user = df1[:user][i]
+            item = df1[:item][i]
+            rating = df1[:rating][i]
+            @test Persa.value(dataset1[user, item]) == rating
+        end
+
+        for i = 1:size(df2)[1]
+            user = df2[:user][i]
+            item = df2[:item][i]
+            rating = df2[:rating][i]
+            @test Persa.value(dataset2[user, item]) == rating
+        end
+
+        for i = 1:length(dataset1)
+            (user, item, rating) = dataset1[i]
+            @test dataset1[user, item] == rating
+        end
+
+        for i = 1:length(dataset2)
+            (user, item, rating) = dataset2[i]
+            @test dataset2[user, item] == rating
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,38 +1,21 @@
 using Persa
 using Test
+using DataFrames
 
-dataset[1] # (1, 1, 5, 2, NaN, 2, NaN)
+function createdummydatasetone()
+    df = DataFrame()
+    df[:user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
+    df[:item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4, 5, 6, 2, 4, 5]
+    df[:rating] = [2.5, 3.5, 3.0, 3.5, 2.5, 3.0, 3, 3.5, 1.5, 5, 3, 3.5, 2.5, 3.0, 3.5, 4.0, 3.5, 3.0, 4.0, 2.5, 4.5, 3.0, 4.0, 2.0, 3.0, 2.0, 3.0, 3.0, 4.0, 5.0, 3.5, 3.0, 4.5, 4.0, 1.0]
+    return df
+end
 
-dataset[1,1] # (Id(1), Id(1), Rating(5, Preferences))
+function createdummydatasettwo()
+    df = DataFrame()
+    df[:user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
+    df[:item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4, 5, 6, 2, 4, 5]
+    df[:rating] = [2, 7, 3, 3, 8, 3, 3, 3, 1, 9, 3, 3, 2, 3, 10, 4, 3, 3, 4, 2, 4, 3, 4, 2, 3, 2, 3, 3, 4, 5, 9, 10, 7, 6, 8]
+    return df
+end
 
-dataset[1,2,1] # Rating(2, Preferences)
-
-dataset[1,1,:] # Context(Rating(2, Preferences), 5, 2, NaN, 2, NaN)
-
-dataset[1,1,:] # Context(Null)
-
-dataset[1,1,:date] # date
-
-contexts(dataset) # [:rating, :date, :day, :humor]
-
-Rating(2, Preferences) + Rating(2, Preferences) = Rating(4, Preferences)
-
-Rating(2, Preferences{Int}) + 2 = Rating(4, Preferences)
-
-Rating(2, Preferences{Int}) + 2.1 = 4.1
-
-Rating(2, Preferences) + 6 = Rating(5, Preferences)
-
-mean([Rating(2, Preferences{Int}), ...]) = 3.5
-
-mean([Rating(2, Preferences{Int}), Rating(Vazio, Preferences{Int}), ...]) = 3.0
-
-Rating(2, Preference) > 2 # false
-
-Rating(2, Preference) + 2 > 2 # true
-
-Rating(2, Preference) + Rating(2, Preference) > 2 # true
-
-Rating(2, Preference) > Rating(2, Preference) # false
-
-Rating(Vazio, Preference) + 1 # Rating(Vazio, Preference)
+include("datasetTest.jl")
