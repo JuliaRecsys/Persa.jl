c3dde3a7935ae2cd1dd74ef40b0cfabf105d2634
autodocs <autodocs>
2018-11-02 03:31:30 +0000

    build based on 1b36ea5

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Persa.jl-1",
+    "page": "Introduction",
+    "title": "Persa.jl",
+    "category": "section",
+    "text": "Persa is a package ..."
+},
+
+]}
