d9a86da0663c4a6af97f703139c482f4a1256880
filipebraida <filipebraida@gmail.com>
2019-08-14 10:59:57 -0300

    Revert "Formating code"
    This reverts commit 2aba5aad32ad8e084401d0f9c6f0d79018bde9af.

--- src/preference.jl
+++ src/preference.jl
@@ -1,10 +1,10 @@
 struct Preference{T}
-    possibles::Array{T,1}
-    min::T
-    max::T
+  possibles::Array{T, 1}
+  min::T
+  max::T
 end
 
-Preference(possibles::Array{T,1}) where T <: Real = Preference(sort(uniquepossiles)), minimum(possibles), maximum(possibles))
+Preference(possibles::Array{T, 1}) where T <: Real = Preference(sort(unique(possibles)), minimum(possibles), maximum(possibles))
 
 Base.string(x::Preference) = string(sort(x.possibles))
 Base.print(io::IO, x::Preference) = print(io, string(x))
@@ -39,27 +39,28 @@ Base.eltype(preference::Preference) = eltype(preference.possibles)
 Returns the nearest integral value of the rating preferences.
 """
 function Base.round(rating::T, preference::Preference{T}) where T
-    ratings = sort(preference.possibles)
+  ratings = sort(preference.possibles)
 
-    m = bs.(rting .- ratings)
+  m = abs.(rating .- ratings)
 
-    return ratings[findall(r->r == minimum(m), m)[end]]
+  return ratings[findall(r->r == minimum(m), m)[end]]
 end
 
 Base.round(rating::Float64, preference::Preference{Int}) = Base.round(Base.convert(Int, round(rating)), preference)
-Base.round(rating, preference::Preference{T}) where T = Base.round(Base.convert(T, rating), prefeence)
+Base.round(rating, preference::Preference{T}) where T = Base.round(Base.convert(T, rating), preference)
+
 """
     correct(rating::Real, preference::Preference)
 Returns the correct value preserving the interval of rating
 preferences.
 """
 function correct(rating::Real, preference::Preference)
-    if rating > maximum(preference)
-        return maximum(preference)
-    elseif rating < minimum(preference)
-        return minimum(preference)
-    end
-    return rating
+  if rating > maximum(preference)
+    return maximum(preference)
+  elseif rating < minimum(preference)
+    return minimum(preference)
+  end
+  return rating
 end
 
-recommendation(preference::Preference) = maximum(preference) - round(1 / 3 * (maximum(preference) -  minimum(preference)))
+recommendation(preference::Preference) = maximum(preference) - round(1/3 * (maximum(preference) -  minimum(preference)))
