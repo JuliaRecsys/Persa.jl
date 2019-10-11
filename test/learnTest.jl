15560f897b512078cccefeeefb12ec8b2ffe45ac
Filipe Braida <filipebraida@gmail.com>
2019-10-11 12:05:59 -0300

    Change learn test

--- test/learnTest.jl
+++ test/learnTest.jl
@@ -1,17 +1,17 @@
+struct RandomModel{T} <: Persa.Model{T}
+    preference::Persa.Preference{T}
+    users::Int
+    items::Int
+end
+
+RandomModel(dataset::Persa.Dataset) = RandomModel(dataset.preference, Persa.users(dataset), Persa.items(dataset))
+
 @testset "Model Learn Tests" begin
     df1 = createdummydatasetone()
     dataset1 = Persa.Dataset(df1)
 
     df2 = createdummydatasettwo()
     dataset2 = Persa.Dataset(df2, 10, 10)
-
-    struct RandomModel{T} <: Persa.Model{T}
-        preference::Persa.Preference{T}
-        users::Int
-        items::Int
-    end
-
-    RandomModel(dataset::Persa.Dataset) = RandomModel(dataset.preference, Persa.users(dataset), Persa.items(dataset))
 
     Persa.predict(model::RandomModel, user::Int, item::Int) = rand(model.preference.possibles)
 
